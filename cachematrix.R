6cd74964ce52d15e5abe0a3caf58389b1e18d4e1
enaeasow <52273834+enaeasow@users.noreply.github.com>
2019-11-20 22:32:48 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -20,12 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
   if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
+    message("getting cached data") # Display message whenever same output is called
+    return(m)                      # return inversed matrix stored as m     
   }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setinverse(m)
+  data <- x$get()  
+  m <- solve(data, ...)  # Function to get inverse of square matrix
+  x$setinverse(m)  # Set the inverted output matrix
   m
 }
 
